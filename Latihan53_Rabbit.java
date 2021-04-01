82b128d7a45d6b17aedec9c2cac5c55faebfaa9e
Selfira <81574694+Selfira17@users.noreply.github.com>
2021-04-01 21:07:36 +0700

    Add files via upload
    Latihan pbo

---
+++ Latihan53_Rabbit.java
@@ -0,0 +1,31 @@
+/*
+ * 
+Nama    : Selfira Anyuniarwati
+Kelas   : PBO (FS112B)
+NIM     : 2031332802
+Deskripsi : Latihan 53-RABBIT
+ */
+package latihan.pkg53_rabbit;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Latihan53_Rabbit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+    System.out.println("Hello,His name is Peter");
+    System.out.println("Peter is Vegetarian? false");
+    System.out.println("Peter eats grass");
+    System.out.println("Peter has 4 legs.");
+    System.out.println("Hello,His name is Peter");
+    System.out.println("Peter color is grey");
+    
+    }
+    
+}
